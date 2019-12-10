eadf68f2b03ec27ea239dffa20268aeb849f5ee4
ben-wycliff <ben12wycliff@gmail.com>
2019-12-10 16:10:48 +0300

    Adds routes in app.js

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const catalogRoutes = require('./routers/catalog');
+const consumerRoutes = require('./routers/consumer');
+const adminRoutes = require('./routers/owner');
+
+
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-app.listen(8080);
+app.use(catalogRoutes);
+app.use(consumerRoutes);
+app.use(adminRoutes);
+
+app.listen(3000);
