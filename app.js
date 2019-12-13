0f7f78f9fe8381a99fe880a997aac81a90056023
ben-wycliff <ben12wycliff@gmail.com>
2019-12-13 16:38:03 +0300

    Updates admin routes

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.use(bodyParser.json());
 
 app.use(catalogRoutes);
 app.use(consumerRoutes);
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+const adminControllers = require('../controllers/admin');
+
+//====================routes===========================
 router.get('/admin', (req, res)=>{
     res.json({message: 'Hello, admin!'});
 })
+router.post('/add-equipment', adminControllers.addItem)
+router.get('/equipment', adminControllers.getItem)
 
 module.exports = router;
