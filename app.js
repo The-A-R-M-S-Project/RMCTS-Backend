ec707013287549035021d963812e6ffecfb68dbd
ben-wycliff <ben12wycliff@gmail.com>
2019-12-13 17:29:11 +0300

    Sets CORS headers

--- app.js
+++ app.js
@@ -11,9 +11,17 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Method','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
 
 app.use(catalogRoutes);
 app.use(consumerRoutes);
+
+//------------------routes-------------------------------
 app.use('/admin', adminRoutes);
 
 app.listen(3000);
