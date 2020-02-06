510bf59f627f3f1b202d8044daab68bda2a4d465
ben-wycliff <ben12wycliff@gmail.com>
2020-02-06 11:53:48 +0300

    Connects to the database using mongoose

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const catalogRoutes = require('./routes/catalog');
 const consumerRoutes = require('./routes/consumer');
 const adminRoutes = require('./routes/admin');
-const connectMongo = require('./utils/database').connectMongo;
+// const connectMongo = require('./utils/database').connectMongo;
 
 
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Method','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Method', 'GET,POST,PUT,PATCH,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     next()
 })
@@ -25,6 +26,15 @@ app.use(catalogRoutes);
 app.use(consumerRoutes);
 app.use('/admin', adminRoutes);
 
-connectMongo(() => {
-    app.listen(3000);
-});
+//------------------establishing connection------------------
+
+// connectMongo(() => {
+//     app.listen(3000);
+// });
+mongoose
+    .connect('mongodb+srv://ben-wycmongodb+srv://ben-wycliff:9UwETeTDtrG5zg7K@rctms-qwi1m.mongodb.net/rmcts?retryWrites=true&w=majorityliff:9UwETeTDtrG5zg7K@rctms-qwi1m.mongodb.net/rmcts?retryWrites=true&w=majority')
+    .then(result => {
+        app.listen(3000);
+    }).catch(err => {
+        console.log(err)
+    })
