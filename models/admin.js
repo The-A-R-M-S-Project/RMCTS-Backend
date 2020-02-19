ec24cd16ae769527cfedaa9098d7830d88043b93
ben-wycliff <ben12wycliff@gmail.com>
2020-02-20 00:53:52 +0300

    Creates admin/user model

---
+++ models/admin.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const adminSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: value => {
+      if (!validator.isEmail(value)) {
+        throw new Error({ error: "Invalid Email address" });
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 7
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
+});
+
+
+// Hashing password before saving the admin model
+adminSchema.pre("save", async function(next) {
+  const admin = this;
+  if (admin.isModified("password")) {
+    admin.password = await bcrypt.hash(admin.password, 8);
+  }
+  next();
+});
+
+// Generating an authentication token for the admin
+adminSchema.methods.generateAuthToken = async function() {
+  const admin = this;
+  const token = jwt.sign({ _id: admin._id }, process.env.JWT_KEY);
+  admin.tokens = admin.tokens.concat({ token });
+  await admin.save();
+  return token;
+};
+
+// Searching for the admin by email and password.
+adminSchema.statics.findByCredentials = async (email, password) => {
+  const admin = await admin.findOne({ email });
+  if (!admin) {
+    throw new Error({ error: "Invalid login credentials" });
+  }
+  const isPasswordMatch = await bcrypt.compare(password, admin.password);
+  if (!isPasswordMatch) {
+    throw new Error({ error: "Invalid login credentials" });
+  }
+  return admin;
+};
+
+const admin = mongoose.model("admin", adminSchema);
+
+module.exports = admin;
