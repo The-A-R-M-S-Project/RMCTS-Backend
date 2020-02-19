fcf55270708f36b6eaf4d14d5cefdbafef892334
ben-wycliff <ben12wycliff@gmail.com>
2020-02-20 02:54:35 +0300

    Formats code with prettier

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const catalogRoutes = require('./routes/catalog');
-const adminRoutes = require('./routes/admin');
+const catalogRoutes = require("./routes/catalog");
+const adminRoutes = require("./routes/admin");
 // const connectMongo = require('./utils/database').connectMongo;
-
-
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Method', 'GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    next()
-})
-
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Method", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 //------------------routes-------------------------------
 app.use(catalogRoutes);
-app.use('/admin', adminRoutes);
+app.use("/admins", adminRoutes);
 
 //------------------establishing connection------------------
 
@@ -30,10 +27,17 @@ app.use('/admin', adminRoutes);
 //     app.listen(3000);
 // });
 mongoose
-    .connect('mongodb+srv://ben-wycliff:49Rb1m1qiikpIFP3@rctms-qwi1m.mongodb.net/test?retryWrites=true&w=majority')
-    .then(result => {
-        app.listen(3000);
-        console.log("your app is running on port 3000")
-    }).catch(err => {
-        console.log(err)
-    })
+  .connect(
+    "mongodb+srv://ben-wycliff:49Rb1m1qiikpIFP3@rctms-qwi1m.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true
+    }
+  )
+  .then(result => {
+    app.listen(3000);
+    console.log("your app is running on port 3000");
+  })
+  .catch(err => {
+    console.log(err);
+  });
