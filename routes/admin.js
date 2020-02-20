c87fbeb30660058dbf988b00b5ce95700f3d48ab
ben-wycliff <ben12wycliff@gmail.com>
2020-02-20 04:59:41 +0300

    Adds logout endpoints

--- routes/admin.js
+++ routes/admin.js
@@ -16,8 +16,28 @@ router.get('/me', auth, async(req, res)=>{
     res.send(req.admin);
 })
 // HTTP POST /admins/logout ---->Logs out the admin
-// HTTP post /admins/logoutall ----> Logs out admin from all devices.
+router.post('/me/logout', auth, async(req, res) => {
+    try {
+        req.admin.tokens = req.admin.tokens.filter((token)=>{
+            return token.token != req.token
+        })
+        await req.admin.save()
+        res.send()
+    } catch(err){
+        res.status(500).send(error)
+    }
+})
 
+// HTTP post /admins/logoutall ----> Logs out admin from all devices.
+router.post('/me/logout-all', auth, async(req, res)=> {
+    try{
+        req.admin.tokens.splice(0, req.admin.tokens.length)
+        await req.user.save()
+        res.send()
+    }catch(error){
+        res.status(500).send(error)
+    }
+})
 router.get('/', (req, res)=>{
     res.json({message: 'Hello, admin!'});
 })
