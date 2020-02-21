0f28b222cf5364c60a1659adb5d6c5eadb22d6dd
ben-wycliff <ben12wycliff@gmail.com>
2020-02-21 11:02:52 +0300

    Cleans comments

--- routes/admin.js
+++ routes/admin.js
@@ -2,20 +2,17 @@ const express = require('express');
 const router = express.Router();
 const adminControllers = require('../controllers/Admin');
 
-const auth = require('../middleware/auth');
+const auth = require('../controllers/auth');
 
 //====================routes===========================
-// HTTP POST /admins ----> Registers admin.
+
 router.post('/', adminControllers.createNewAdmin)
 
-// HTTP POST /admins/login ----> Allow admins to login.
 router.post('/login', adminControllers.adminLogin)
 
-// HTTP GET / admins/me ----> Gets admin profile.
 router.get('/me', auth, async(req, res)=>{
     res.send(req.admin);
 })
-// HTTP POST /admins/logout ---->Logs out the admin
 router.post('/me/logout', auth, async(req, res) => {
     try {
         req.admin.tokens = req.admin.tokens.filter((token)=>{
