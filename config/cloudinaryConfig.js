2794d7d5bbe6e2fda07d7e49c37c4f570b49a5ac
ben-wycliff <ben12wycliff@gmail.com>
2020-04-18 19:11:30 +0300

    Changes export format

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -2,12 +2,15 @@ const cloudinary = require('cloudinary')
 require('dotenv').config();
 
 const config = cloudinary.config
-const uploader = cloudinary.uploader
+exports.uploader = cloudinary.uploader
 
-const cloudinaryConfig = () => config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
-});
-
-module.exports = { cloudinaryConfig, uploader };
+exports.cloudinaryConfig = (req, res, next) => {
+    config({
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.API_KEY,
+        api_secret: process.env.API_SECRET,
+    });
+    next()
+}
+// exports.cloudinaryConfig
+// exports.uploader
--- middlewares/multer.js
+++ middlewares/multer.js
@@ -3,11 +3,9 @@ const DataURI = require('datauri')
 const path = require('path')
 
 const storage = multer.memoryStorage();
-const multerUploads = multer({ storage }).single('image');
+exports.multerUploads = multer({ storage }).single('image');
 
 const dURI = new DataURI();
 
 // converting buffer to dataURI
-const dataURI = req => dURI.format(path.extname(req.file.originalname).toString(), req.file.buffer)
-
-module.exports =  {multerUploads, dataURI};
+exports.dataURI = req => dURI.format(path.extname(req.file.originalname).toString(), req.file.buffer)
