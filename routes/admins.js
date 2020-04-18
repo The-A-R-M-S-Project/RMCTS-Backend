be487728b08c868b5f9b737c37955879a9b7ac31
ben-wycliff <ben12wycliff@gmail.com>
2020-04-18 19:13:57 +0300

    Adds multer middleware

--- routes/admins.js
+++ routes/admins.js
@@ -2,43 +2,42 @@ const express = require('express');
 const router = express.Router();
 const adminControllers = require('../controllers/Admins');
 
-const auth = require('../controllers/auth');
+const auth = require('../middlewares/auth');
+const multer = require('../middlewares/multer')
 
 //====================routes===========================
 
-router.post('/', adminControllers.createNewAdmin)
+router.post('/', multer.multerUploads, adminControllers.createNewAdmin)
 
 router.post('/login', adminControllers.adminLogin)
 
-router.get('/me', auth, async(req, res)=>{
+router.get('/me', auth, async (req, res) => {
     res.send(req.admin);
 })
-router.post('/me/logout', auth, async(req, res) => {
+router.post('/me/logout', auth, async (req, res) => {
     try {
-        req.admin.tokens = req.admin.tokens.filter((token)=>{
+        req.admin.tokens = req.admin.tokens.filter((token) => {
             return token.token != req.token
         })
         await req.admin.save()
         res.send()
-    } catch(err){
+    } catch (err) {
         res.status(500).send(error)
     }
 })
 
 // HTTP post /admins/logoutall ----> Logs out admin from all devices.
-router.post('/me/logout-all', auth, async(req, res)=> {
-    try{
+router.post('/me/logout-all', auth, async (req, res) => {
+    try {
         req.admin.tokens.splice(0, req.admin.tokens.length)
         await req.admin.save()
         res.send()
-    }catch(error){
+    } catch (error) {
         res.status(500).send(error)
     }
 })
-router.get('/', (req, res)=>{
-    res.json({message: 'Hello, admin!'});
+router.get('/', (req, res) => {
+    res.json({ message: 'Hello, admin!' });
 })
-// router.post('/add-equipment', adminControllers.addItem)
-// router.get('/equipment', adminControllers.getItem)
 
 module.exports = router;
