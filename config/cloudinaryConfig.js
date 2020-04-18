1677ccab5d045e98ded6551366cca8143f632069
ben-wycliff <ben12wycliff@gmail.com>
2020-04-18 15:58:44 +0300

    Creates cloudinary config

---
+++ config/cloudinaryConfig.js
@@ -0,0 +1,12 @@
+const cloudinary = require('cloudinary')
+
+const config = cloudinary.config
+const uploader = cloudinary.uploader
+
+const cloudinaryConfig = () => config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+});
+
+module.exports = { cloudinaryConfig, uploader };
