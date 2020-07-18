fba32794dcf77b62d762f6d4ac4c032e79e8810b
ben-wycliff <benclifg@gmail.com>
2020-07-18 15:58:34 +0300

    [REFACTOR] Consistent with async functions

--- controllers/Admins.js
+++ controllers/Admins.js
@@ -3,28 +3,30 @@ const multer = require('../middlewares/multer')
 const cloudinary = require('../config/cloudinaryConfig')
 
 
-exports.createNewAdmin = (req, res) => {
-
+exports.createNewAdmin = async(req, res) => {
+  try{
   const adm = req.body
-  const file = multer.dataURI(req).content;
+  const file = await multer.dataURI(req).content;
 
-  cloudinary.uploader.upload(file).then((result) => {
-    adm.profileImage = result.secure_url;
-    adm.profileImageID = result.public_id
+  await cloudinary.uploader.upload(file)
+  adm.profileImage = result.secure_url;
+  adm.profileImageID = result.public_id
 
-    const admin = new Admin(adm);
-    admin.save();
+  const admin = new Admin(adm);
+  await admin.save();
 
-    return res.status(200).json({
-      message: 'Your account has been created successfully',
-      admin: admin
-    })
-  }).catch((err) => res.status(400).json({
+  return res.status(200).json({
+    message: 'Your account has been created successfully',
+    admin: admin
+  })
+  }
+  catch(err){
+    res.status(400).json({
     message: 'someting went wrong while processing your request',
     data: {
       err
     }
-  }))
+  })}
 };
 
 exports.adminLogin = async (req, res) => {
