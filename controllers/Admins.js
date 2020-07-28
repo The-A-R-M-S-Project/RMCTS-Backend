782a8c235f267bdfa0a1af4f239d9c3710e30c06
ben-wycliff <benclifg@gmail.com>
2020-07-28 16:05:01 +0300

    Sends verification token upon signup

--- controllers/Admins.js
+++ controllers/Admins.js
@@ -1,6 +1,8 @@
 const Admin = require("../models/admin");
 const multer = require("../middlewares/multer");
 const cloudinary = require("../config/cloudinaryConfig");
+const { TokenExpiredError } = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 
 exports.createNewAdmin = async (req, res) => {
   try {
@@ -11,13 +13,71 @@ exports.createNewAdmin = async (req, res) => {
     adm.profileImage = result.secure_url;
     adm.profileImageID = result.public_id;
 
-    const admin = new Admin(adm);
-    await admin.save();
+    // Cheking if account already exists
+    Admin.findOne({ email: adm.email }, async function (err, admin) {
+      if (admin)
+        return res.status(400).send({
+          msg:
+            "The email address you have entered is already associated with another account",
+        });
 
-    return res.status(200).json({
-      message: "Your account has been created successfully",
-      admin: admin,
+      // Create account
+      admin = new Admin(adm);
+      await admin.save(function (err) {
+        if (err) {
+          return res.status(500).send({ msg: err.message });
+        }
+
+        // Create a verification token for this account
+        var token = new Token({
+          _userId: admin._id,
+          token: crypto.randomBytes(16).toString("hex"),
+        });
+
+        // Save the verification token
+        token.save(function (err) {
+          if (err) {
+            return res.status(500).send({ msg: err.message });
+          }
+
+          // Send email
+          let transporter = nodemailer.createTransport({
+            servie: "Sendgrid",
+            auth: {
+              user: process.env.EMAIL_ACCOUNT,
+              pass: process.env.EMAIL_PASSWORD,
+            },
+          });
+          let mailOptions = {
+            from: "no-reply@rmcts.com",
+            to: admin.email,
+            subject: "Account Verification Token",
+            text:
+              "Hello,\n\n" +
+              "Please verify your account by clicking the link: \nhttp://" +
+              req.headers.host +
+              "/confirmation/" +
+              token.token +
+              ".\n",
+          };
+          transporter.sendMail(mailOptions, function (err) {
+            if (err) {
+              return res.status(500).send({ msg: err.message });
+            }
+            res
+              .status(200)
+              .send(
+                "A verification email has been sent to " + admin.email + "."
+              );
+          });
+        });
+      });
     });
+
+    // return res.status(200).json({
+    //   message: "Your account has been created successfully",
+    //   admin: admin,
+    // });
   } catch (err) {
     res.status(400).json({
       message: "someting went wrong while processing your request",
@@ -38,12 +98,10 @@ exports.adminLogin = async (req, res) => {
         .send({ error: "Login failed! Check authentication credentials" });
     }
     if (!admin.isVerified)
-      return res
-        .status(401)
-        .send({
-          type: "not-verified",
-          msg: "This account has not been verified",
-        });
+      return res.status(401).send({
+        type: "not-verified",
+        msg: "This account has not been verified",
+      });
     const token = await admin.generateAuthToken();
     res.send({ admin, token });
   } catch (error) {
@@ -73,3 +131,5 @@ exports.logoutAll = async (req, res) => {
     res.status(500).send(error);
   }
 };
+
+exports.confirmEmail = async (req, res) => {};
