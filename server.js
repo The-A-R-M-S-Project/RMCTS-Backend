4a3abba77217c1dfe0bf71d56f5e11897c5e3813
ben-wycliff <benclifg@gmail.com>
2020-08-01 18:03:16 +0300

    Set database URL depending on environment

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+if (process.env.NODE_ENV !== "test") {
+  process.on("uncaughtException", (err) => {
+    console.log("UNCAUGHT EXCEPTION");
+    console.log(err.name, err.message);
+    process.exit(1);
+  });
+}
 
-if (process.env.NODE_ENV !== "test"){
-    process.on('uncaughtException', err => {
-        console.log("UNCAUGHT EXCEPTION");
-        console.log(err.name, err.message);
-        process.exit(1);
-    })
+dotenv.config();
+//connects server to app.js
+const app = require("./app");
+
+// configure databases
+
+let DB =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_PRODUCTION
+    : process.env.DATABASE_LOCAL;
+
+if (process.env.NODE_ENV === "test"){
+    DB = process.env.DATABASE_TEST;
 }
