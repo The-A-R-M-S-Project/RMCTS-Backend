ee7ba3601867bcd13bd4dee1c756c8b1e9451d22
ben-wycliff <benclifg@gmail.com>
2020-08-03 01:04:45 +0300

    Initial test code

---
+++ test/data.js
@@ -0,0 +1,13 @@
+exports.institution1 = {
+    role: "institution",
+    username: "ARMS",
+    email: "arms143mro@gmail.com",
+    password: "123456789"
+}
+
+exports.institution2 = {
+    role: "institution",
+    username: "ARMS2",
+    email: "ben12wycliff@gmail.com",
+    password: "123456789"
+}
---
+++ test/test_user.js
@@ -0,0 +1,40 @@
+process.env.NODE_ENV = "test";
+
+const User = require("../models/user");
+const server = require("../server");
+const data = require("./data");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+
+chai.use(chaiHttp);
+const client = chai.request.agent(server);
+const should = chai.should();
+
+describe("User", () => {
+  beforeEach((done) => {
+    User.deleteMany({}, () => {});
+    done();
+  });
+
+  afterEach((done) => {
+    User.deleteMany({}, () => {});
+    done();
+  });
+
+  describe("/POST /users", () => {
+    it("Should successfully create new Institution account and send verification token", async () => {
+      const { username, email, password } = data.institution2;
+
+      const signupPromise = new Promise((resolve, response) => {
+        client
+          .post("/users/")
+          .send({ username, email, password })
+          .then((res) => resolve(res));
+      });
+
+      const res = await signupPromise;
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+    });
+  });
+});
