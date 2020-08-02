4d9a07bc7c18e7b8e568e8a975a4f664edf952e4
ben-wycliff <benclifg@gmail.com>
2020-08-02 12:32:05 +0300

    Creates token sign and send functions

--- auth/authController.js
+++ auth/authController.js
@@ -1,21 +1,42 @@
-const jwt = require('jsonwebtoken');
-const Admin = require('../models/admin');
+const jwt = require("jsonwebtoken");
+const Admin = require("../models/admin");
 
-const auth = async(req, res, next) => {
-    const token = req.headers.authorization.replace('Bearer ', '');
 
-    const data = jwt.verify(token, process.env.JWT_KEY);
-    try {
-        const admin = await Admin.findOne({_id: data._id, 'tokens.token': token})
-        if (!admin) {
-            throw new Error();
-        }
-        req.admin = admin;
-        req.token = token;
-        next();
-    } catch (error) {
-        res.status(401).send({error: 'Your\'re not authorised to access this'});
-    }
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+    issuer: process.env.JWT_ISSUER,
+  });
+};
+
+
+const SendToken = (user, status, res) => {
+  const token = signToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 60000),
+    httpOnly: true, // send token via HTTPOnly cookie
+  };
+
+  res.cookie("jwt", token, cookieOptions);
+
+  user.password = undefined; // Remove user password before output
+  res.status(status).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
+
+class AuthController {
+
+  constructor(User) {
+    this.User = User;
+  }
+
+  
 }
-
-module.exports = auth;
+module.exports = AuthController;
