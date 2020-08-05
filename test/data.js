b5129dd425e569e4f75ec14212ecefe5e7695f9b
ben-wycliff <benclifg@gmail.com>
2020-08-05 12:27:45 +0300

    Adds Institution login tests

--- test/data.js
+++ test/data.js
@@ -1,13 +1,22 @@
-exports.institution1 = {
-    role: "institution",
-    username: "ARMS",
-    email: "arms143mro@gmail.com",
-    password: "123456789"
-}
-
 exports.institution2 = {
     role: "institution",
     username: "ARMS2",
     email: "ben12wycliff@gmail.com",
     password: "123456789"
+}
+
+exports.verifiedInstitution = {
+    role: "institution",
+    username: "ARMS2",
+    email: "institution@gmail.com",
+    password: "123456789",
+    isVerified: true
+}
+
+exports.unVerifiedInstitution = {
+    role: "institution",
+    username: "ARMS2",
+    email: "institution@gmail.com",
+    password: "123456789",
+    isVerified: false
 }
--- test/test_institution.js
+++ test/test_institution.js
@@ -1,6 +1,7 @@
 process.env.NODE_ENV = "test";
 
 const User = require("../models/user");
+const Token = require("../models/token");
 const server = require("../server");
 const data = require("./data");
 const chai = require("chai");
@@ -10,14 +11,22 @@ chai.use(chaiHttp);
 const client = chai.request.agent(server);
 const should = chai.should();
 
+const createUser = async (data) => {
+  const user = new User(data);
+  await user.save();
+  return user;
+};
+
 describe("User", () => {
   beforeEach((done) => {
     User.deleteMany({}, () => {});
+    Token.deleteMany({}, () => {});
     done();
   });
 
   afterEach((done) => {
     User.deleteMany({}, () => {});
+    Token.deleteMany({}, () => {});
     done();
   });
 
@@ -33,24 +42,91 @@ describe("User", () => {
       });
 
       const res = await signupPromise;
-        res.should.have.status(200);
-        res.body.should.be.a("object");
+      res.should.have.status(200);
+      res.body.should.be.a("object");
+      res.body.should.have
+        .property("msg")
+        .eq("A verification email has been sent to " + email + ".");
     });
   });
 
-  describe("/POST /user/login", () => {
-    it("Should succesfully login verified user", async (req, res) => {
-      const { username, email, password } = data.verifiedUser;
+  describe("/POST /users/login", () => {
+    it("Should succesfully login verified account", async () => {
+      await createUser(data.verifiedInstitution);
+      const { email, password } = data.verifiedInstitution;
 
-      const signupPromise = new Promise((resolve, response) => {
+      const loginPromise = new Promise((resolve, response) => {
         client
-          .post("/users/")
-          .send({ username, email, password })
-          .then((res) => resolve(res));
+          .post("/users/login")
+          .send({ email, password })
+          .then((res) => {
+            resolve(res);
+          });
       });
 
-      await signupPromise;
-      
-    })
-  })
+      const res = await loginPromise;
+      res.should.have.status(200);
+      res.body.should.be.a("object");
+      res.body.should.have.property("status").eq("success");
+      res.body.should.have.property("token");
+      res.body.should.have.property("data");
+    });
+  });
+
+  describe("/POST /users/login", () => {
+    it("Should succesfully detect unverified account", async () => {
+      await createUser(data.unVerifiedInstitution);
+
+      const { email, password } = data.unVerifiedInstitution;
+
+      const loginPromise = new Promise((resolve, response) => {
+        client
+          .post("/users/login")
+          .send({ email, password })
+          .then((res) => {
+            resolve(res);
+          });
+      });
+
+      const res = await loginPromise;
+      res.should.have.status(401);
+      res.body.should.be.a("object");
+      res.body.should.have.property("type").eq("not-verified");
+      res.body.should.have
+        .property("msg")
+        .eq("This account has not been verified");
+    });
+  });
+
+  describe("GET /users/me", () => {
+    it("Should return account's details", async () => {
+      await createUser(data.verifiedInstitution);
+      const { email, password, username } = data.verifiedInstitution;
+
+      const loginPromise = new Promise((resolve, response) => {
+        client
+          .post("/users/login")
+          .send({ email, password })
+          .then((res) => {
+            resolve(res);
+          });
+      });
+      const loginResponse = await loginPromise;
+      const getMePromise = new Promise((resolve, response) => {
+        client
+          .get("/users/me")
+          .set("Authorization", `Bearer ${loginResponse.body.token}`)
+          .then((res) => {
+            resolve(res);
+          });
+      });
+
+      const res = await getMePromise;
+      res.should.have.status(200);
+      res.body.should.be.a("object");
+      res.body.should.have.property("username").eq(username);
+      res.body.should.have.property("email").eq(email);
+      res.body.should.have.property("role").eq("institution");
+    });
+  });
 });
