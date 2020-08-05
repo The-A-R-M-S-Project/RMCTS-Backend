ecd1e60eb673cd590087deca444a83298671fe3e
ben-wycliff <benclifg@gmail.com>
2020-08-05 15:40:50 +0300

    Adds item testing data

--- test/data.js
+++ test/data.js
@@ -30,3 +30,59 @@ exports.updatedProfile = {
     password: "123456789",
     websiteURL: ""
 }
+
+exports.item1={
+    title: "Laptop",
+    location: "Makindye, Kampala",
+    description: "cool specs",
+    _id: "5f2713b129aa8b2e60aa5804"
+}
+exports.item2={
+    title: "Laptop2",
+    location: "Makerere, Kampala",
+    description: "cool specs",
+    _id: "5f2713b129aa8b2e60aa5803"
+
+}
+exports.item3={
+    title: "Computer labaratory",
+    location: "Ntinda, Kampala",
+    description: "Huge, super fast computers",
+    _id: "5f2713b129aa8b2e60aa5802"
+
+}
+exports.item4={
+    title: "Conference Hall",
+    location: "Munyonyo, Kampala",
+    description: "cool, huge, authentic",
+    _id: "5f2713b129aa8b2e60aa5801"
+
+}
+exports.item5={
+    title: "Work space",
+    location: "Ntinda, Kampala",
+    description: "Open, fresh, fully stocked",
+    _id: "5f2713b129aa8b2e60aa5800"
+
+}
+exports.item6={
+    title: "Work Bench",
+    location: "Makerere, Kampala",
+    description: "Electronic",
+    _id: "5f2713b129aa8b2e60aa5799"
+
+}
+exports.item7={
+    title: "GPU",
+    location: "Nakawa, Kampala",
+    description: "Lightening speed",
+    _id: "5f2713b129aa8b2e60aa5798"
+
+}
+exports.image1 = "./images/image1.jpg";
+exports.image2 = "./images/image2.jpg";
+exports.image3 = "./images/image3.jpg";
+exports.image4 = "./images/image4.jpg";
+exports.image5 = "./images/image5.jpg";
+exports.image6 = "./images/image6.jpg";
+exports.image7 = "./images/image7.jpg";
