d9976349446d41f134eead4f1a9f87af0bbd7f6d
ben-wycliff <benclifg@gmail.com>
2020-08-05 18:52:53 +0300

    Moves create user to utils folder

--- test/equipment.js
+++ test/equipment.js
@@ -6,7 +6,8 @@ const server = require("../server");
 const data = require("./data");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const { createUser } = require("../scripts/createuser");
+const fs = require("fs");
+const { createUser } = require("../utils/createuser");
 
 chai.use(chaiHttp);
 const client = chai.request.agent(server);
@@ -25,5 +26,40 @@ describe("Equipment", () => {
     done();
   });
 
-  
+  describe("POST /equipment/item", () => {
+    it("Should add new item", async () => {
+      const { verifiedInstitution, item1, image1 } = data;
+      const { email, password } = verifiedInstitution;
+      createUser(verifiedInstitution);
+
+      const loginPromise = new Promise((resolve) => {
+        client
+          .post("/users/login")
+          .send({ email, password })
+          .then((res) => resolve(res));
+      });
+
+      const loginResponse = await loginPromise;
+      const addItemPromise = new Promise((resolve) => {
+        client
+          .post("/equipment/item")
+          .set("Authorization", `Bearer ${loginResponse.body.token}`)
+          .field("_id", item1._id)
+          .field("title", item1.title)
+          .field("location", item1.location)
+          .field("description", item1.description)
+          .attach("image", image1, image1.split("/")[-1])
+          .then((res) => resolve(res));
+      });
+
+      const res = await addItemPromise;
+      res.should.have.status(201);
+      res.should.be.a("object");
+      res.body.should.have.property("title").eq(item1.title);
+      res.body.should.have.property("description").eq(item1.description);
+      res.body.should.have.property("location").eq(item1.location);
+      res.body.should.have.property("imageURL");
+      res.body.should.have.property("imageID");
+    });
+  });
 });
